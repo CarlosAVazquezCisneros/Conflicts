2bb9bf232870089285f112cb29e9843f48330c43
carlos <carlos.alberto.vazquez.cisneros@uabc.edu.mx>
2023-05-29 20:39:56 -0700

    Primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+name = "Alberto"
+messange = f"Hola {name}"
+print(messange)
+
+age = input("Dime tu edad: ")
+
+print("Tu edad es: " + age)
+
+estatura = input("Dime tu estatura: ")
+
+print(f"Tu nombre es {name} y tu edad es {age} tu estatura es {age}")
+
